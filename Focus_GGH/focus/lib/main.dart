2a2582604fe7fe1d73fc909ff3df64cdc21c7dc8
YumikoooD <mikael.sourati@epitech.eu>
2023-11-11 18:57:51 +0100

    UX focus change

--- Focus_GGH/focus/lib/main.dart
+++ Focus_GGH/focus/lib/main.dart
@@ -5,6 +5,16 @@ void main() {
   runApp(MyApp());
 }
 
+BoxDecoration _buildGradientDecoration() {
+  return BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [Colors.white, Colors.purple[300]!],
+    ),
+  );
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,7 +22,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Roboto',
         primarySwatch: Colors.purple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
     );
@@ -23,17 +32,28 @@ class TextPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Text Page'),
+      appBar: PreferredSize(
+        preferredSize:
+            Size.fromHeight(kToolbarHeight), // Taille standard de la AppBar
+        child: AppBar(
+          backgroundColor: _buildGradientDecoration().color,
+          elevation: 0,
+          flexibleSpace: Container(
+            decoration: _buildGradientDecoration(),
+          ),
+          title: Container(
+            height: 60.0,
+            alignment: Alignment.center,
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Text(
-          'Plus petit, je revvais d\'en avoir un comme animal de compagnie. Mais je me suis rendu compte que c\'était impossible. En effet, les dauphins sont des animaux sauvages et ils ne peuvent pas vivre en captivité. Ils ont besoin de beaucoup d\'espace pour nager et de beaucoup de nourriture. Ils sont aussi très sensibles au bruit et à la pollution. Ils ne peuvent pas vivre dans un aquarium. LEs dauphins sont des animaux marins, ils sont très intelligents et très sociables. Plus petit, je revvais d\'en avoir un comme animal de compagnie. Mais je me suis rendu compte que c\'était impossible. En effet, les dauphins sont des animaux sauvages et ils ne peuvent pas vivre en captivité. Ils ont besoin de beaucoup d\'espace pour nager et de beaucoup de nourriture. Ils sont aussi très sensibles au bruit et à la pollution. Ils ne peuvent pas vivre dans un aquarium. LEs dauphins sont des animaux marins, ils sont très intelligents et très sociables. Plus petit, je revvais d\'en avoir un comme animal de compagnie. Mais je me suis rendu compte que c\'était impossible. En effet, les dauphins sont des animaux sauvages et ils ne peuvent pas vivre en captivité. Ils ont besoin de beaucoup d\'espace pour nager et de beaucoup de nourriture. Ils sont aussi très sensibles au bruit et à la pollution. Ils ne peuvent pas vivre dans un aquarium. LEs dauphins sont des animaux marins, ils sont très intelligents et très sociables. Plus petit, je revvais d\'en avoir un comme animal de compagnie. Mais je me suis rendu compte que c\'était impossible. En effet, les dauphins sont des animaux sauvages et ils ne peuvent pas vivre en captivité. Ils ont besoin de beaucoup d\'espace pour nager et de beaucoup de nourriture. Ils sont aussi très sensibles au bruit et à la pollution. Ils ne peuvent pas vivre dans un aquarium. LEs dauphins sont des animaux marins, ils sont très intelligents et très sociables. Plus petit, je revvais d\'en avoir un comme animal de compagnie. Mais je me suis rendu compte que c\'était impossible. En effet, les dauphins sont des animaux sauvages et ils ne peuvent pas vivre en captivité. Ils ont besoin de beaucoup d\'espace pour nager et de beaucoup de nourriture. Ils sont aussi très sensibles au bruit et à la pollution. Ils ne peuvent pas vivre dans un aquarium.',
+          'Le projet darrêté, émanant des services du secrétaire dEtat à la Mer Hervé Berville, affirme que les systèmes de caméras "sont susceptibles dapporter une contribution significative" à la lutte contre les captures accidentelles. "La visibilité apportée aux professionnels de la pêche sur plusieurs années est un facteur déterminant de leur engagement à s’équiper en dispositifs techniques de sélectivité", estime le texte.Le projet darrêté, émanant des services du secrétaire dEtat à la Mer Hervé Berville, affirme que les systèmes de caméras "sont susceptibles dapporter une contribution significative" à la lutte contre les captures accidentelles. "La visibilité apportée aux professionnels de la pêche sur plusieurs années est un facteur déterminant de leur engagement à séquiper en dispositifs techniques ou en système dobservation électronique à distance", justifie encore le texte dans son préambule. La période de fermeture envisagée ne concerne pas le golfe du Morbihan ni le bassin dArcachon.Le projet darrêté, émanant des services du secrétaire dEtat',
           textAlign: TextAlign.center,
           style: TextStyle(
             fontSize: 16.0,
-            fontWeight: FontWeight.bold,
           ),
         ),
       ),
@@ -46,60 +66,40 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: const Color.fromARGB(255, 170, 117, 190),
         elevation: 0, // Aucune ombre
         title: Center(
           child: Container(
             height: 60.0,
-            child: Image.asset('assets/images/Logo_Focus.png', fit: BoxFit.contain),
+            child: Image.asset('assets/images/Logo_Focus.png',
+                fit: BoxFit.contain),
           ),
         ),
       ),
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [Colors.white, Colors.purple[300]!], // Gradient plus doux
-          ),
-        ),
+        decoration: _buildGradientDecoration(), // Utilisez le dégradé ici
         child: Center(
           child: Hero(
             tag: 'hero',
-            child: Card(
-              elevation: 5, // Légère ombre pour le card
-              margin: EdgeInsets.all(16), // Espacement autour du card
-              child: ListTile(
-                leading: Icon(Icons.home, color: Colors.purple[800]),
-                title: Text(
-                  'Hello, World!',
-                  style: TextStyle(
-                    color: Colors.purple[800],
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold, // Texte en gras
-                  ),
-                ),
-                trailing: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.purple, // Couleur du bouton
-                    foregroundColor: Colors.white, // Couleur du texte du bouton
-                  ),
-                  child: Text('Focus!'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => TextPage()),
-                    );
-                  },
-                ),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.purple[300], // Couleur du bouton
+                foregroundColor: Colors.white, // Couleur du texte du bouton
               ),
+              child: Text('Focus!'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TextPage()),
+                );
+              },
             ),
           ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed, // Fixe le bar
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.purple[300],
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.purple[100],
         items: const <BottomNavigationBarItem>[
